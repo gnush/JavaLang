501bd7406c792bfe484448a20c05302e0734e171
gnush <gnush@hushmail.com>
2014-11-20 11:53:38 +0100

    changed constraint-generation for names to support fields (partial) and fixed and error in the result set merging (applied the mgu to the binding requirements and declarations)

--- test/foo.java
+++ test/foo.java
@@ -1,15 +1,4 @@
-public class A{
-    private int field;
-    
-    public A(){
-        field = 0;
-    }
-    
-    public A(int field){
-        this.field = field;
-    }
-    
-    public int getField(Obj o){
-        return o.field;
-    }
+{
+    int i;
+    i = 0;
 }
